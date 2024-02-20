fbd49bc4f7dfa53e058f179f9d39da792cc9f545
Nicoás HG <116156113+NicolasHG-ULL@users.noreply.github.com>
2024-02-20 16:56:13 +0000

    Correción en la creeación del informe

--- src/informe.py
+++ src/informe.py
@@ -1,12 +1,18 @@
-from ydata_profiling import ProfileReport
 import streamlit as st
 
-from streamlit_pandas_profiling import st_profile_report
+from ydata_profiling import ProfileReport
 
 def mostrar_informe(df):
     try:
-        report = ProfileReport(df)
-        st_profile_report(report)
+        # Generar informe de perfil
+        profile = ProfileReport(df, title="Profiling Report")
+
+        # Convertir el informe de perfil en HTML
+        html_report = profile.to_html()
+
+        # Mostrar el informe en Streamlit
+        st.title("Informe")
+        st.components.v1.html(html_report, width=800, height=600, scrolling=True)
     except Exception as e:
-        st.error("Se produjo el siguiente error al calcular la matriz de correlación:")    
+        st.error("Se produjo el siguiente error:")    
         st.error(e)
